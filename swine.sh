a430e182154ec2eab63966b7c3f394cafcab2455
Andrew Gallagher <andrew.gallagher@siren.io>
2020-07-21 10:36:17 +0100

    implement contains()

--- swine.sh
+++ swine.sh
@@ -2,20 +2,36 @@
 # Make bash a little bit more like perl. Turn on strict error handling, and
 # implement some of the more useful perl native functions.
 # See: https://perldoc.perl.org/5.32.0/index-functions.html
+#
 # This file has no magic number, and is not executable.
 # THIS IS INTENTIONAL as it should never be executed, only sourced.
+#
+# . ${POSHLIB}/swine.sh || exit 101
 ################################################################################
 
-set -euo pipefail;
+set -euo pipefail
 err_report() {
     echo "errexit on line $(caller)" >&2
 };
 trap err_report ERR
 
-# DIE takes two arguments, unlike perl
-# die <errnum> <string>
+# die takes two arguments, unlike perl
+# do something || die $errnum "$notice"
 
 die() {
     echo $2 >&2
     exit $1
+}
+
+# if contains "$element" "${array[@]}"; then ...
+
+contains() {
+    local i element="$1"
+    shift
+    for i; do
+        if [[ "$i" == "$element" ]]; then
+            return 0
+        fi
+    done
+    return 1
 }
