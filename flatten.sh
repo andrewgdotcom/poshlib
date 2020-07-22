691028ef17afa7775a88f563a50019078977ba40
Andrew Gallagher <andrew.gallagher@siren.io>
2020-07-22 15:10:33 +0100

    Remove bashisms, echo

--- flatten.sh
+++ flatten.sh
@@ -23,13 +23,12 @@ flatten() { (
         local dir
         local IFS=:
         for dir in $USEPATH; do
-            if [[ -f "$dir/$1.sh" ]]; then
+            if [ -f "$dir/$1.sh" ]; then
                 flatten "$dir/$1.sh"
                 return 0
             fi
         done
-        echo "Could not find $1.sh in $USEPATH" 2>&1
-        exit 101
+        die 101 "Could not find $1.sh in $USEPATH"
     }
 
     outfile=$(mktemp) || die 1001 "Could not open temporary file"
@@ -37,19 +36,19 @@ flatten() { (
 
     exec <"$1"
     while IFS= read -r input; do
-        if module=$(awk '$1=="use" {print $2}' <<<"$input"); [[ $module && ! $continuation ]]; then
-            echo "# BEGIN FLATTEN USE $module" >> $outfile
+        if module=$(awk '$1=="use" {print $2}' <<<"$input"); [ $module -a ! $continuation ]; then
+            say "# BEGIN FLATTEN USE $module" >> $outfile
             cat $(recurse "$module") >> $outfile
-            echo "# END FLATTEN USE $module" >> $outfile
+            say "# END FLATTEN USE $module" >> $outfile
         else
-            echo "$input" >> $outfile
+            say "$input" >> $outfile
         fi
-        if [[ "${input%\\}" != "${input}" ]]; then
+        if [ "${input%\\}" != "${input}" ]; then
             continuation=1
         else
             continuation=
         fi
     done
 
-    echo "$outfile"
+    say "$outfile"
 ) }
