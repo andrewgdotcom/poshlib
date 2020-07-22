3ad453c780d55ce4b72edbb4a2df8c3acf86b5fd
Andrew Gallagher <andrew.gallagher@siren.io>
2020-07-22 15:11:56 +0100

    Fail on too many arguments

--- swine.sh
+++ swine.sh
@@ -19,29 +19,43 @@ trap err_report ERR
 # The default behaviour of `echo` differs between shells, so we deprecate it.
 # say (from perl6/raku) forces there to be no escape-char handling at runtime.
 # If we want to interpret escape sequences in literals then we should use $''.
-# If we want to interpret them at runtime we should pass through $(echo -e).
+# If we want to interpret them at runtime we should explicitly use $(echo -e).
 
 say() {
-    echo -E "$1"
+    local text="$1"; shift
+    if [ "$*" != "" ]; then
+        die 102 "Too many arguments to say() at line $(caller)"
+    fi
+    echo -E "$text"
 }
+
+# warn() is the same as perl
 
 warn() {
-    echo -E "$1" >&2
+    local text="$1"; shift
+    if [ "$*" != "" ]; then
+        die "Too many arguments to warn() at line $(caller)"
+    fi
+    echo -E "$text" >&2
 }
 
-# die takes two arguments, unlike perl
+# die() takes two arguments, unlike perl
 # do something || die $errnum "$notice"
 
 die() {
-    echo -E "$2" >&2
-    exit "$1"
+    local errcode="$1"; shift
+    local text="$1"; shift
+    if [ "$*" != "" ]; then
+        die "Too many arguments to die() at line $(caller)"
+    fi
+    echo -E "$text" >&2
+    exit "$errcode"
 }
 
 # if contains "$element" "${array[@]}"; then ...
 
 contains() {
-    local i element="$1"
-    shift
+    local i element="$1"; shift
     for i; do
         if [ "$i" == "$element" ]; then
             return 0
