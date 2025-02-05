51697449176362ee6a3ed273738d37b99ec962e7
Andrew Gallagher <andrewg@andrewg.com>
2025-02-05 09:58:01 +0000

    delete swine

--- swine.sh
+++
@@ -1,9 +0,0 @@
-# shellcheck disable=SC2148
-################################################################################
-# This file has no magic number, and is not executable.
-# THIS IS INTENTIONAL as it should never be executed, only sourced.
-################################################################################
-
-# `swine` is just a backwards-compatibility layer for `strict` + `utils`
-use strict
-use utils
